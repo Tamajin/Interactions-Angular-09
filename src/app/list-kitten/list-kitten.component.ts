515f454003333ec665944aca0dedd8d3aca9956a
Tamajin <wild.jlq@gmail.com>
2022-11-09 09:39:05 +0100

    gestion de la liste de chats

--- src/app/list-kitten/list-kitten.component.ts
+++ src/app/list-kitten/list-kitten.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { FieldsForm } from '../models/KittenCreateForm';
 
 @Component({
   selector: 'app-list-kitten',
   templateUrl: './list-kitten.component.html',
-  styleUrls: ['./list-kitten.component.css']
+  styleUrls: ['./list-kitten.component.css'],
 })
 export class ListKittenComponent implements OnInit {
+  @Input()
+  cats = {
+    name: '',
+    race: '',
+    birthday: '',
+    picture: '',
+    isMouseOver: false,
+  };
 
-  constructor() { }
+  @Output()
+  CatsAdopted: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit(): void {
+  @Input()
+  catsList!: FieldsForm[];
+
+  @Output()
+  isMouseOver: EventEmitter<boolean> = new EventEmitter();
+
+  @Output()
+  isMouseOut: EventEmitter<boolean> = new EventEmitter();
+
+  setMouseOver(): void {
+    this.isMouseOver.emit(true);
   }
 
+  setMouseOut(): void {
+    this.isMouseOut.emit(true);
+  }
+
+  adoption(): void {
+    this.CatsAdopted.emit(this.catsList);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
